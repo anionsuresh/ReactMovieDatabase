a547bc19fda339abdaa95910d21017b16a8158ad
Anirudh Suresh <141362228+anionsuresh@users.noreply.github.com>
2024-01-11 23:03:08 +0530

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,7 +3,7 @@ function Banner() {
     <div className="h-[20vh] md:h-[60vh] bg-center flex items-end bg-cover" style={{
       backgroundImage: "url(https://wallpapercave.com/wp/wp9995111.jpg)"
     }}>
-      <div className="text-xl md:text-3xl bg-gray-900 bg-opacity-60 p-4 text-white w-full">Aspirants</div>
+      <div className="text-xl md:text-3xl bg-gray-900 bg-opacity-60 p-4 text-white w-full">OnePiece</div>
     </div>
   );
 }
